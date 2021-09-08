1a7f8a22918a9aee26bd08ce32d75b48aa5996f5
Dylan Rowe <dylanthk@gmail.com>
2021-09-08 11:52:50 -0500

    adding edit

--- src/components/DataProvider.js
+++ src/components/DataProvider.js
@@ -7,6 +7,8 @@ export const DataProvider = (props) => {
     const [movie, setMovie] = useState([])
     const [dbFavMovies, setDbFavMovie] = useState([])
     const [favoriteMovie, setFavoritedMovie] = useState({})
+    const [reviewedMovies, setReviewedMovies] = useState([])
+    console.log(reviewedMovies, 'provider')
 
     const fetchMoviesJSON = async (title) => {
         try {
@@ -35,6 +37,34 @@ export const DataProvider = (props) => {
         }
     }
 
+    const reviewedMoviePost = async (review) => {
+        try {
+            const response = await fetch('http://localhost:8080/review', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(review)
+            })
+
+            const reviewData = await response.json()
+            return getReviewedMovies(reviewData)
+
+        } catch (error) {
+            window.alert('error')
+        }
+    }
+
+    const getReviewedMovies = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/review`);
+            const movies = await response.json()
+            setReviewedMovies(movies)
+        } catch (error) {
+            console.error(error)
+        }
+    };
+
     const fetchNewMoviesById = async (movieId) => {
         try {
             const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${movieApikey}`);
@@ -60,11 +90,17 @@ export const DataProvider = (props) => {
             method: 'DELETE',
         }).then(getFavoriteMovies)
     }
+    const releaseReview = (id) => {
+        return fetch(`http://localhost:8080/review/${id}`, {
+            method: 'DELETE',
+        }).then(getReviewedMovies)
+    }
 
     return (
         <DataContext.Provider value={{
-            movie, fetchMoviesJSON, fetchNewMoviesById, favoriteMovie, 
-            favoriteMoviePost, dbFavMovies, getFavoriteMovies, releaseFavoriteMovie
+            movie, fetchMoviesJSON, fetchNewMoviesById, favoriteMovie,
+            favoriteMoviePost, dbFavMovies, getFavoriteMovies, releaseFavoriteMovie,
+            reviewedMovies, getReviewedMovies, reviewedMoviePost, releaseReview
         }} >
             {props.children}
         </DataContext.Provider>
--- src/components/moviedetails.js
+++ src/components/moviedetails.js
@@ -1,16 +1,32 @@
 import React from "react";
 import { Container, Image, Col } from "react-bootstrap";
-import { MovieArray } from "../data/data";
+import { useContext, useEffect } from "react";
+import { DataContext } from "./DataProvider";
+import { RatingView } from 'react-simple-star-rating'
 
 export const MovieReview = (props) => {
-  const movieId = parseInt(props.match.params.movieId);
-  const filterMovie = MovieArray.find((movie) => movie.id === movieId);
+
+  const { reviewedMovies, getReviewedMovies } = useContext(DataContext)
+
+  useEffect(() => {
+    getReviewedMovies()
+  }, [])
+
+  const { movieId } = props.match.params
+
+  const found = reviewedMovies.find(movie => movie.movieId === parseInt(movieId))
+
+  const path = found.poster
+
   return (
     <Container>
       <Col className='filteredImageContainer'>
-        <Image src={filterMovie.image} className='filteredImage' fluid />
+        <Image className='filteredImage' fluid src={"https://image.tmdb.org/t/p/w500/" + path} />
       </Col>
-      <p className='reviewText'>{filterMovie.review}</p>
+      <Col className="d-flex justify-content-center">
+        <RatingView ratingValue={found.rating} />
+      </Col>
+      <p className='reviewText text-center'>{found.review}</p>
     </Container>
   );
 };
